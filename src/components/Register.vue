6e047775d13df4b91c9982e50dfbe58446ef5ac8
Stoorx <antiproton2013@yandex.ru>
2020-02-29 23:24:35 +0300

    Add Register.vue

---
+++ src/components/Register.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="root">
+        <div class="name">{{name}}</div>
+        <div class="value">{{value}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Vue} from 'vue-property-decorator'
+
+  @Component
+  export default class Register extends Vue {
+    @Prop()
+    private name!: string
+
+    @Prop()
+    private value!: string
+  }
+</script>
+
+<style lang="scss" scoped>
+    $paddingSize: 7px;
+    $borderRadius: 4px;
+    $borderColor: #404040;
+    $nameBgColor: #C0C0C0;
+    $textColor: #202020;
+
+    .root {
+        color: $textColor;
+        margin: 2px;
+        font-family: Monospaced, monospace;
+        font-size: 12pt;
+        display: flex;
+        flex-direction: row;
+        border-radius: $borderRadius;
+        border: 1px solid $borderColor;
+        overflow: hidden;
+
+        .name {
+            border-right: solid 1px $borderColor;
+            padding: $paddingSize;
+            background: $nameBgColor;
+            font-weight: bolder;
+        }
+
+        .value {
+            padding: $paddingSize;
+            text-align: center;
+            flex-grow: 1;
+        }
+    }
+</style>
---
+++ src/views/BcompView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        BComp view
+        <Register name="AC" value="0xDEAD"/>
+        <Register name="BR" value="0xBABA"/>
+        <Register name="PS" value="0x0101"/>
+        <Register name="DR" value="0xDDDD"/>
+        <Register name="CR" value="0xC000"/>
+        <Register name="IP" value="0x0C00"/>
+        <Register name="SP" value="0xABAC"/>
+        <Register name="AR" value="0x0122"/>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator'
+  import Register from '@/components/Register.vue'
+
+  @Component({
+    components: {
+      Register
+    }
+  })
+  export default class BcompView extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
